a8d204a62b76aa544dd4442d17b66de9208061cc
Will Jones <willjones2010@gmail.com>
2019-03-04 21:13:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1,array.length);
+  return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
